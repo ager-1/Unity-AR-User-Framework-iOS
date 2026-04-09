a23e108d8863574e8fc96f43ee65b03474193ca1
Ager <aghazofficial@gmail.com>
2026-04-09 16:31:39 +0800

    Update ARFramework.cs

--- Assets/Scripts/ARFramework.cs
+++ Assets/Scripts/ARFramework.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class ARFramework<T>: MonoBehaviour where T: ARFramework<T>
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static T Instance { get; private set; }
+    public static bool IsInitialized
     {
-        
+        get { return Instance != null; }
+    }
+    protected virtual void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError($"Trying to instantiate a second instance of singleton class {GetType().Name}");
+        }
+        else
+        {
+            Instance = (T)this;
+        }
+    }
+    protected virtual void OnDestroy()
+    {
+        if (Instance == this) Instance = null;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
